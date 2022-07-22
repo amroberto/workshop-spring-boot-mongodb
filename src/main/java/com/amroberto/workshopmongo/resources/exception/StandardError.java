84d028cd3d2744089e02baa6702bc65fea7795ea
Alexandre Martins Roberto <amroberto@gmail.com>
2022-07-22 10:25:44 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/amroberto/workshopmongo/resources/exception/StandardError.java
@@ -0,0 +1,67 @@
+package com.amroberto.workshopmongo.resources.exception;
+
+import java.io.Serializable;
+
+public class StandardError implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Long timestamp;
+	private Integer status;
+	private String Error;
+	private String message;
+	private String Path;
+	
+	public StandardError() {
+	}
+
+	public StandardError(Long timestamp, Integer status, String error, String message, String path) {
+		super();
+		this.timestamp = timestamp;
+		this.status = status;
+		Error = error;
+		this.message = message;
+		Path = path;
+	}
+
+	public Long getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Long timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getError() {
+		return Error;
+	}
+
+	public void setError(String error) {
+		Error = error;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getPath() {
+		return Path;
+	}
+
+	public void setPath(String path) {
+		Path = path;
+	}
+	
+	
+}
